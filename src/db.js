be97960141a46c937984a698c27dba10bc522933
coderwessel <nospam917@gmail.com>
2019-01-13 10:33:20 +0100

    package updates

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 //actions/games.js
 //development
-// export const baseUrl = 'http://localhost:4000'
+export const baseUrl = 'http://localhost:4000'
 //production
-export const baseUrl = 'https://a0hh1api.herokuapp.com'
+// export const baseUrl = 'https://a0hh1api.herokuapp.com'
